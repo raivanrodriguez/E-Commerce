39e305af3bc841e10a6f7ca6c1045d90c5826a0d
raivanrodriguez <raivanrodriguez@icloud.com>
2022-08-21 17:52:44 -0300

    Entrega 1

--- js/products.js
+++ js/products.js
@@ -9,15 +9,24 @@ fetch(autos_url)
  .then(datos =>{
    for (let leer of datos.products){
     
-     let row= `
-     
-     <img style="float:left;height:10em;width:auto;margin-right:20px;" src="`+ leer.image +` "</img>
-     
-     <h4> ` + leer.name + " " + "-" + " "+ leer.currency + leer.cost +`  </h4>
-     <p style="text-align:right;" <div class="text-muted"> </div> 
-     ` + leer.soldCount + "    " + "Vendidos" +` <p/>
-     
-     <p style="margin-bottom:90px;"> ` + leer.description + `</p>
+      let row= `
+      <div onclick="setCatID(${leer.id})" class="list-group-item list-group-item-action cursor-active">
+  
+        <div class="row">
+           <div class="col-3">
+              <img src="` + leer.image + `" class="img-thumbnail">
+            </div>
+            <div class="col">
+              <div class="d-flex w-100 justify-content-between">
+                <h4 class="mb-1"> ` + leer.name + " " + "-" + " " +leer.currency + " " + leer.cost + ` </h4>
+                <small class="text-muted"> `+ leer.soldCount +` artículos</small>
+             
+              </div>
+              <p class="mb-1">`+ leer.description +`</p>
+            </div>
+        </div>
+      </div>
+
      
      
      
